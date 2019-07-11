6004b9af1986d60b72ab1205ef8ac40ded5bc315
Nickolai Zeldovich <nickolai@csail.mit.edu>
2019-07-11 19:09:16 -0400

    c_api: factor out common logic for importing a variable-length buffer

--- src/pairings/c_api.rs
+++ src/pairings/c_api.rs
@@ -67,15 +67,15 @@ fn return_g1(g1: &super::G1) -> *mut ffi::c_void {
   Box::into_raw(g1box) as *mut ffi::c_void
 }
 
+fn vcp_value_slice<'a>(vv: &vcp_value) -> &'a [u8] {
+  unsafe { slice::from_raw_parts(vv.buf, vv.buflen) }
+}
+
 #[no_mangle]
 pub extern fn vcp_commit(prover: *const ffi::c_void, values: *const vcp_value, nvalues: libc::size_t) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
   let pvalues = unsafe { slice::from_raw_parts(values, nvalues) };
-  let mut vvalues = Vec::new();
-  for i in 0..nvalues {
-    let v = unsafe { slice::from_raw_parts(pvalues[i].buf, pvalues[i].buflen) };
-    vvalues.push(v);
-  }
+  let vvalues: Vec<_> = pvalues.iter().map(vcp_value_slice).collect();
 
   let com = super::commit::commit(pprover, &vvalues);
   return_g1(&com)
@@ -85,11 +85,7 @@ pub extern fn vcp_commit(prover: *const ffi::c_void, values: *const vcp_value, n
 pub extern fn vcp_prove(prover: *const ffi::c_void, values: *const vcp_value, nvalues: libc::size_t, idx: libc::size_t) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
   let pvalues = unsafe { slice::from_raw_parts(values, nvalues) };
-  let mut vvalues = Vec::new();
-  for i in 0..nvalues {
-    let v = unsafe { slice::from_raw_parts(pvalues[i].buf, pvalues[i].buflen) };
-    vvalues.push(v);
-  }
+  let vvalues: Vec<_> = pvalues.iter().map(vcp_value_slice).collect();
 
   let proof = super::prove::prove(pprover, &vvalues, idx);
   return_g1(&proof)
@@ -99,8 +95,8 @@ pub extern fn vcp_prove(prover: *const ffi::c_void, values: *const vcp_value, nv
 pub extern fn vcp_proof_update(prover: *const ffi::c_void, proof: *const ffi::c_void, idx: libc::size_t, changed_idx: libc::size_t, val_old: vcp_value, val_new: vcp_value) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
   let pproof = unsafe { &*(proof as *const super::G1) };
-  let value_before = unsafe { slice::from_raw_parts(val_old.buf, val_old.buflen) };
-  let value_after = unsafe { slice::from_raw_parts(val_new.buf, val_new.buflen) };
+  let value_before = vcp_value_slice(&val_old);
+  let value_after = vcp_value_slice(&val_new);
 
   let newproof = super::prove::proof_update(pprover, pproof, idx, changed_idx, value_before, value_after);
   return_g1(&newproof)
@@ -110,8 +106,8 @@ pub extern fn vcp_proof_update(prover: *const ffi::c_void, proof: *const ffi::c_
 pub extern fn vcp_commit_update(prover: *const ffi::c_void, com: *const ffi::c_void, changed_idx: libc::size_t, val_old: vcp_value, val_new: vcp_value) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
   let pcom = unsafe { &*(com as *const super::G1) };
-  let value_before = unsafe { slice::from_raw_parts(val_old.buf, val_old.buflen) };
-  let value_after = unsafe { slice::from_raw_parts(val_new.buf, val_new.buflen) };
+  let value_before = vcp_value_slice(&val_old);
+  let value_after = vcp_value_slice(&val_new);
 
   let newcom = super::commit::commit_update(pprover, pcom, changed_idx, value_before, value_after);
   return_g1(&newcom)
@@ -122,21 +118,7 @@ pub extern fn vcp_verify(verifier: *const ffi::c_void, com: *const ffi::c_void, 
   let pverifier = unsafe { &*(verifier as *const super::VerifierParams) };
   let pcom = unsafe { &*(com as *const super::G1) };
   let pproof = unsafe { &*(proof as *const super::G1) };
-  let val = unsafe { slice::from_raw_parts(val.buf, val.buflen) };
+  let val = vcp_value_slice(&val);
 
   super::verify::verify(pverifier, pcom, pproof, val, idx)
-}
-
-#[no_mangle]
-pub extern fn vcp_bench_bytes_to_g1(comptr: *const u8) {
-  let combuf = unsafe { slice::from_raw_parts(comptr, 48) };
-  super::prove::convert_bytes_to_proof(&combuf);
-}
-
-#[no_mangle]
-pub extern fn vcp_bench_bytes_to_g1_to_bytes(comptr: *const u8, pout: *mut u8) {
-  let combuf = unsafe { slice::from_raw_parts(comptr, 48) };
-  let com = super::prove::convert_bytes_to_proof(&combuf);
-  let mut out = unsafe { slice::from_raw_parts_mut(pout, 48) };
-  super::prove::write_proof_into_slice(&com, &mut out);
 }
