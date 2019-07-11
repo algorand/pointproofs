548f40f266fd012a22efb58ae5390ccd39d5bad3
Nickolai Zeldovich <nickolai@csail.mit.edu>
2019-07-11 14:30:35 -0400

    change the C API: keep commitments/proofs as G1

--- src/pairings/c_api.rs
+++ src/pairings/c_api.rs
@@ -42,7 +42,33 @@ pub extern fn vcp_free_verifier_params(vp: *mut ffi::c_void) {
 }
 
 #[no_mangle]
-pub extern fn vcp_commit(prover: *const ffi::c_void, values: *const vcp_value, nvalues: libc::size_t, pout: *mut u8) {
+pub extern fn vcp_free_g1(g1: *mut ffi::c_void) {
+  unsafe {
+    Box::from_raw(g1);
+  }
+}
+
+#[no_mangle]
+pub extern fn vcp_g1_to_bytes(g1: *const ffi::c_void, pout: *mut u8) {
+  let pg1 = unsafe { &*(g1 as *const super::G1) };
+  let mut out = unsafe { slice::from_raw_parts_mut(pout, 48) };
+  super::prove::write_proof_into_slice(pg1, &mut out);
+}
+
+#[no_mangle]
+pub extern fn vcp_g1_from_bytes(buf: *const u8) -> *mut ffi::c_void {
+  let g1buf = unsafe { slice::from_raw_parts(buf, 48) };
+  let g1 = super::prove::convert_bytes_to_proof(&g1buf);
+  return_g1(&g1)
+}
+
+fn return_g1(g1: &super::G1) -> *mut ffi::c_void {
+  let g1box = Box::new(*g1);
+  Box::into_raw(g1box) as *mut ffi::c_void
+}
+
+#[no_mangle]
+pub extern fn vcp_commit(prover: *const ffi::c_void, values: *const vcp_value, nvalues: libc::size_t) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
   let pvalues = unsafe { slice::from_raw_parts(values, nvalues) };
   let mut vvalues = Vec::new();
@@ -52,12 +78,11 @@ pub extern fn vcp_commit(prover: *const ffi::c_void, values: *const vcp_value, n
   }
 
   let com = super::commit::commit(pprover, &vvalues);
-  let mut out = unsafe { slice::from_raw_parts_mut(pout, 48) };
-  super::prove::write_proof_into_slice(&com, &mut out);
+  return_g1(&com)
 }
 
 #[no_mangle]
-pub extern fn vcp_prove(prover: *const ffi::c_void, values: *const vcp_value, nvalues: libc::size_t, idx: libc::size_t, pout: *mut u8) {
+pub extern fn vcp_prove(prover: *const ffi::c_void, values: *const vcp_value, nvalues: libc::size_t, idx: libc::size_t) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
   let pvalues = unsafe { slice::from_raw_parts(values, nvalues) };
   let mut vvalues = Vec::new();
@@ -67,49 +92,39 @@ pub extern fn vcp_prove(prover: *const ffi::c_void, values: *const vcp_value, nv
   }
 
   let proof = super::prove::prove(pprover, &vvalues, idx);
-  let mut out = unsafe { slice::from_raw_parts_mut(pout, 48) };
-  super::prove::write_proof_into_slice(&proof, &mut out);
+  return_g1(&proof)
 }
 
 #[no_mangle]
-pub extern fn vcp_proof_update(prover: *const ffi::c_void, proof: *const u8, idx: libc::size_t, changed_idx: libc::size_t, val_old: vcp_value, val_new: vcp_value, pout: *mut u8) {
+pub extern fn vcp_proof_update(prover: *const ffi::c_void, proof: *const ffi::c_void, idx: libc::size_t, changed_idx: libc::size_t, val_old: vcp_value, val_new: vcp_value) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
-  let proofbuf = unsafe { slice::from_raw_parts(proof, 48) };
+  let pproof = unsafe { &*(proof as *const super::G1) };
   let value_before = unsafe { slice::from_raw_parts(val_old.buf, val_old.buflen) };
   let value_after = unsafe { slice::from_raw_parts(val_new.buf, val_new.buflen) };
 
-  let proof = super::prove::convert_bytes_to_proof(&proofbuf);
-
-  let newproof = super::prove::proof_update(pprover, &proof, idx, changed_idx, value_before, value_after);
-  let mut out = unsafe { slice::from_raw_parts_mut(pout, 48) };
-  super::prove::write_proof_into_slice(&newproof, &mut out);
+  let newproof = super::prove::proof_update(pprover, pproof, idx, changed_idx, value_before, value_after);
+  return_g1(&newproof)
 }
 
 #[no_mangle]
-pub extern fn vcp_commit_update(prover: *const ffi::c_void, comptr: *const u8, changed_idx: libc::size_t, val_old: vcp_value, val_new: vcp_value, pout: *mut u8) {
+pub extern fn vcp_commit_update(prover: *const ffi::c_void, com: *const ffi::c_void, changed_idx: libc::size_t, val_old: vcp_value, val_new: vcp_value) -> *mut ffi::c_void {
   let pprover = unsafe { &*(prover as *const super::ProverParams) };
-  let combuf = unsafe { slice::from_raw_parts(comptr, 48) };
+  let pcom = unsafe { &*(com as *const super::G1) };
   let value_before = unsafe { slice::from_raw_parts(val_old.buf, val_old.buflen) };
   let value_after = unsafe { slice::from_raw_parts(val_new.buf, val_new.buflen) };
 
-  let com = super::prove::convert_bytes_to_proof(&combuf);
-
-  let newcom = super::commit::commit_update(pprover, &com, changed_idx, value_before, value_after);
-  let mut out = unsafe { slice::from_raw_parts_mut(pout, 48) };
-  super::prove::write_proof_into_slice(&newcom, &mut out);
+  let newcom = super::commit::commit_update(pprover, pcom, changed_idx, value_before, value_after);
+  return_g1(&newcom)
 }
 
 #[no_mangle]
-pub extern fn vcp_verify(verifier: *const ffi::c_void, comptr: *const u8, proofptr: *const u8, val: vcp_value, idx: libc::size_t) -> bool {
+pub extern fn vcp_verify(verifier: *const ffi::c_void, com: *const ffi::c_void, proof: *const ffi::c_void, val: vcp_value, idx: libc::size_t) -> bool {
   let pverifier = unsafe { &*(verifier as *const super::VerifierParams) };
-  let combuf = unsafe { slice::from_raw_parts(comptr, 48) };
-  let proofbuf = unsafe { slice::from_raw_parts(proofptr, 48) };
+  let pcom = unsafe { &*(com as *const super::G1) };
+  let pproof = unsafe { &*(proof as *const super::G1) };
   let val = unsafe { slice::from_raw_parts(val.buf, val.buflen) };
 
-  let com = super::prove::convert_bytes_to_proof(&combuf);
-  let proof = super::prove::convert_bytes_to_proof(&proofbuf);
-
-  super::verify::verify(pverifier, &com, &proof, val, idx)
+  super::verify::verify(pverifier, pcom, pproof, val, idx)
 }
 
 #[no_mangle]
--- wrappers.go
+++ wrappers.go
@@ -20,10 +20,18 @@ type Verifier struct {
 	ptr unsafe.Pointer
 }
 
-type Commitment [48]byte
+// G1 is veccom::pairings::G1
+type G1 struct {
+	ptr unsafe.Pointer
+}
+
+type Commitment struct {
+	G1 *G1
+}
+
 type Proof struct {
 	Index int
-	Proof [48]byte
+	G1 *G1
 }
 
 func ParamGen(seed []uint8, n int) (p *Prover, v *Verifier) {
@@ -46,6 +54,10 @@ func release_verifier(v *Verifier) {
 	C.vcp_free_verifier_params(v.ptr)
 }
 
+func release_g1(g1 *G1) {
+	C.vcp_free_g1(g1.ptr)
+}
+
 func vcp_value(buf []byte) C.vcp_value {
 	return C.vcp_value{
 		buf:    (*C.uchar)(&buf[0]),
@@ -62,48 +74,56 @@ func vcp_values(vals [][]byte) *C.vcp_value {
 	return p
 }
 
-func (p *Prover) Commit(vals [][]byte) (out Commitment) {
+func (p *Prover) Commit(vals [][]byte) Commitment {
 	valbufs := vcp_values(vals)
 	defer C.free(unsafe.Pointer(valbufs))
 
-	C.vcp_commit(p.ptr, valbufs, C.size_t(len(vals)), (*C.uchar)(&out[0]))
-	return
+	res := C.vcp_commit(p.ptr, valbufs, C.size_t(len(vals)))
+	g1 := &G1{res}
+	runtime.SetFinalizer(g1, release_g1)
+	return Commitment{g1}
 }
 
-func (p *Prover) Prove(vals [][]byte, idx int) (out Proof) {
+func (p *Prover) Prove(vals [][]byte, idx int) Proof {
 	valbufs := vcp_values(vals)
 	defer C.free(unsafe.Pointer(valbufs))
 
-	C.vcp_prove(p.ptr, valbufs, C.size_t(len(vals)), C.size_t(idx), (*C.uchar)(&out.Proof[0]))
-	out.Index = idx
-	return
+	res := C.vcp_prove(p.ptr, valbufs, C.size_t(len(vals)), C.size_t(idx))
+	g1 := &G1{res}
+	runtime.SetFinalizer(g1, release_g1)
+	return Proof{Index: idx, G1: g1}
 }
 
 func (v *Verifier) Verify(com Commitment, proof Proof, val []byte) bool {
-	r := C.vcp_verify(v.ptr, (*C.uchar)(&com[0]), (*C.uchar)(&proof.Proof[0]),
+	r := C.vcp_verify(v.ptr, com.G1.ptr, proof.G1.ptr,
 		vcp_value(val), C.size_t(proof.Index))
 	return bool(r)
 }
 
-func (p *Prover) ProofUpdate(proof Proof, changedidx int, oldval []byte, newval []byte) (out Proof) {
-	C.vcp_proof_update(p.ptr, (*C.uchar)(&proof.Proof[0]), C.size_t(proof.Index), C.size_t(changedidx),
-		vcp_value(oldval), vcp_value(newval), (*C.uchar)(&out.Proof[0]))
-	out.Index = proof.Index
-	return
+func (p *Prover) ProofUpdate(proof Proof, changedidx int, oldval []byte, newval []byte) Proof {
+	res := C.vcp_proof_update(p.ptr, proof.G1.ptr, C.size_t(proof.Index), C.size_t(changedidx),
+		vcp_value(oldval), vcp_value(newval))
+	g1 := &G1{res}
+	runtime.SetFinalizer(g1, release_g1)
+	return Proof{Index: proof.Index, G1: g1}
 }
 
-func (p *Prover) CommitUpdate(com Commitment, changedidx int, oldval []byte, newval []byte) (out Commitment) {
-	C.vcp_commit_update(p.ptr, (*C.uchar)(&com[0]), C.size_t(changedidx),
-		vcp_value(oldval), vcp_value(newval), (*C.uchar)(&out[0]))
-	return
+func (p *Prover) CommitUpdate(com Commitment, changedidx int, oldval []byte, newval []byte) Commitment {
+	res := C.vcp_commit_update(p.ptr, com.G1.ptr, C.size_t(changedidx),
+		vcp_value(oldval), vcp_value(newval))
+	g1 := &G1{res}
+	runtime.SetFinalizer(g1, release_g1)
+	return Commitment{g1}
 }
 
-func benchBytesToG1ToBytes(com Commitment) (out Commitment) {
-	C.vcp_bench_bytes_to_g1_to_bytes((*C.uchar)(&com[0]), (*C.uchar)(&out[0]))
-	return
+func BytesToG1(buf [48]byte) *G1 {
+	res := C.vcp_g1_from_bytes((*C.u_char)(&buf[0]))
+	g1 := &G1{res}
+	runtime.SetFinalizer(g1, release_g1)
+	return g1
 }
 
-func benchBytesToG1(com Commitment) {
-	C.vcp_bench_bytes_to_g1((*C.uchar)(&com[0]))
+func (g1 *G1) ToBytes() (out [48]byte) {
+	C.vcp_g1_to_bytes(g1.ptr, (*C.u_char)(&out[0]))
 	return
 }
