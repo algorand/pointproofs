d4322a08d34007e119a1219f8609ac98a6e5244f
zhenfei <zhenfei.zhang@hotmail.com>
2019-12-06 14:29:55 -0500

    remove merkle

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,9 @@ extern crate bigint;
 extern crate ff;
 extern crate pairing_plus as pairing;
 extern crate sha2;
-extern crate typenum;
+
 //extern crate veccom_paramgen;
 
-pub mod merkle;
 pub mod pairings;
 
 #[cfg(test)]
