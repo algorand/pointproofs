effd1a97f8605d2043468d90b891893e74910f48
zhenfei <zhenfei.zhang@hotmail.com>
2019-12-16 15:05:38 -0500

    progress

--- src/bin/gen_param.rs
+++ src/bin/gen_param.rs
@@ -1,16 +1,23 @@
+extern crate ff;
 extern crate pairing_plus as pairing;
 extern crate veccom;
+
 use pairing::serdes::SerDes;
 use veccom::pairings::param::*;
+
 fn main() {
     println!("WARNING!!!");
     println!("WARNING!!!");
     println!("WARNING!!!");
     println!("Parameters generated in this crate is INSECURE. Do NOT use it in production");
 
-    let test_dim = [1024usize, 32_768]; //, 16384, 65536, 262144];
+    let test_dim = [256usize]; //, 16384, 65536, 262144];
 
     for i in &test_dim {
+        #[cfg(not(unswitch_group))]
+        println!("generating testing parameters for {} with proof in G2", i);
+        #[cfg(unswitch_group)]
+        println!("generating testing parameters for {} with proof in G1", i);
         let seed = "this is a very very long seed for testing. Do not use in product";
 
         let (pp, vp) = paramgen_from_seed(seed, 0, *i).unwrap();
