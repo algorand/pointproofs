28b701d3d9edf4e1175526b9b92120b385231935
zhenfei <zhenfei.zhang@hotmail.com>
2019-12-18 19:38:55 -0500

    Update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![allow(clippy::cognitive_complexity)]
-
+#![cfg_attr(feature = "cargo-clippy", allow(clippy::cognitive_complexity))]
+#![cfg_attr(feature = "cargo-clippy", allow(clippy::missing_safety_doc))]
 extern crate bigint;
 extern crate ff;
 extern crate pairing_plus as pairing;
