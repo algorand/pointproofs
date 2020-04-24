dcdaf1c95fa3719e7abb7582651c353e5996b998
zhenfei <zhenfei.zhang@hotmail.com>
2020-04-24 10:25:18 -0400

    switch from ff to ff-zeroize

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::cognitive_complexity))]
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::missing_safety_doc))]
 extern crate bigint;
-extern crate ff;
+extern crate ff_zeroize as ff;
 extern crate pairing_plus as pairing;
 extern crate pointproofs_paramgen;
 extern crate sha2;
